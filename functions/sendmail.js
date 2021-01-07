b3a66f04d0765f8f8a14f458cbf128d9c548a535
mart cube <martingjorceski@gmail.com>
2021-01-07 16:55:13 +0100

    casting form email template

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -3,16 +3,11 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 exports.handler = async (event, context, callback) => {
 	const form = JSON.parse(event.body)
-
-	let template
-	if (form.action === 'Contact') template = `<h4>email:</h4> <p>${form.email}</p> <h4>message:</h4> <p>${form.message}</p>`
-	else if (form.action === 'Casting') template = `<h4>it works</h4> <p>${form.email}</p>`
-
 	const data = {
 		to: process.env.DOMAIN,
 		from: process.env.DOMAIN,
 		subject: form.action,
-		html: template,
+		html: form.emailTemplate,
 	}
 	let response
 
