f0a7b101c690fbd48720be0d5645fc05d93f878f
mart cube <martingjorceski@gmail.com>
2021-01-12 14:06:05 +0100

    video.js component

---
+++ components/Plyr.vue
@@ -0,0 +1,32 @@
+<template>
+	<client-only>
+		<vue-plyr ref="plyr">
+			<video autoplay muted crossorigin playsinline>
+				<source size="576" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" type="video/mp4" />
+				<source size="720" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4" type="video/mp4" />
+				<source size="1080" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4" type="video/mp4" />
+			</video>
+		</vue-plyr>
+	</client-only>
+</template>
+
+<style lang="scss" scoped>
+.plyr--video .plyr__controls,
+.plyr--full-ui.plyr--video .plyr__control--overlaid {
+	display: none !important;
+}
+
+.plyr--video {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	width: 100vw;
+	height: 100vh;
+	transform: translate(-50%, -50%);
+	object-fit: cover;
+	z-index: 0;
+}
+video {
+	object-fit: cover;
+}
+</style>
---
+++ components/VideoItem.vue
@@ -0,0 +1,51 @@
+<template>
+	<video ref="videoPlayer" class="video-js"></video>
+</template>
+
+<script>
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
+
+export default {
+	data: () => ({
+		player: null,
+		options: {
+			autoplay: 'muted',
+			controls: false,
+			loop: true,
+			sources: [
+				{
+					src: 'video.mp4',
+					type: 'video/mp4',
+				},
+			],
+		},
+	}),
+	mounted() {
+		this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady() {
+			console.log('onPlayerReady', this)
+		})
+	},
+	beforeDestroy() {
+		if (this.player) {
+			this.player.dispose()
+		}
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+#vjs_video_3 {
+	width: calc(100vw - 80px);
+	height: calc(100vh - 80px);
+	video {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		width: 100%;
+		height: 100%;
+		transform: translate(-50%, -50%);
+		object-fit: cover;
+	}
+}
+</style>
